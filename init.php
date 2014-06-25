1724715ac8c8a65b86f9e0265c08f1173ddedb65
phoenixMagoo <phoenixMagoo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-25 19:53:08 +0000

    pushing version 2.2
    git-svn-id: https://plugins.svn.wordpress.org/jquery-masonry-image-gallery/trunk@938706 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- init.php
+++ init.php
@@ -5,7 +5,7 @@
 Plugin Name:  jQuery Masonry Image Gallery
 Plugin URI:   http://willrees.com/2013/02/jquery-masonry-and-native-wordpress-image-galleries/
 Description:  Injects jQuery Masonry for native WordPress image galleries. jQuery Masonry is included in WordPress, use it for image galleries. Works best on galleries <strong>without</strong> 1:1 scaled thumbnails.
-Version:      2.1.6
+Version:      2.2
 Author:       Will Rees
 Author URI:   http://willrees.com
 License:
@@ -13,7 +13,7 @@ License:
   Copyright 2014 Will Rees (rees.will@gmail.com)
 
   This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License, version 2, as 
+  it under the terms of the GNU General Public License, version 2, as
   published by the Free Software Foundation.
 
   This program is distributed in the hope that it will be useful,
@@ -24,70 +24,122 @@ License:
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-  
+
 */
 
 if (is_admin()) {
 
 	add_action('admin_init', 'jmig_options_init' );
 	add_action('admin_menu', 'jmig_options_add_page');
-	
+
 		// Init plugin options to white list our options
 		function jmig_options_init(){
 			register_setting( 'jmig_options_options', 'jmig_option', 'jmig_options_validate' );
 		}
-		
+
 		// Add menu page
 		function jmig_options_add_page() {
 			add_options_page('jQuery Masonry Image Gallery Options', 'JMIG Options', 'manage_options', 'jmig_options', 'jmig_options_do_page');
 		}
-		
+
 		// Draw the menu page itself
 		function jmig_options_do_page() {
-		
+
 		global $select_options, $radio_options;
-		
+
 		if ( ! isset( $_REQUEST['settings-updated'] ) )
-		
+
 		$_REQUEST['settings-updated'] = false;
-		
+
 ?>
 
 			<div class="wrap">
-				
+
+				<style>
+
+					#jmig_option_item_margin {width: 2em !important;}
+
+				</style>
+
+
 				<h2>jQuery Masonry Image Gallery Options</h2>
-				
+
 					<form method="post" action="options.php">
-					
+
 						<?php settings_fields('jmig_options_options'); ?>
 						<?php $jmig_options = get_option('jmig_option'); ?>
-					
+
 							<table class="form-table">
-					
-								<p>Check this box <strong>ONLY</strong> if you need to maintain the column count in the WordPress gallery short code. Might be necessary in some themes.</p>
-						
-									<tr valign="top"><th scope="row"><strong>DO NOT</strong> allow Masonry to layout your gallery columns?</th>
-							
-										<td><input name="jmig_option[fixed_layout]" type="checkbox" value="1" <?php checked( '1', (isset($jmig_options['fixed_layout'])) ); ?> /></td>
-									
-									</tr>
-						
+
+								<tr valign="top">
+
+									<th scope="row"><?php _e( 'Gallery Item Margin (in pixels)', 'jmig_plugin' ); ?></th>
+
+										<td>
+
+											<input id="jmig_option_item_margin" class="regular-text" type="text" name="jmig_option[item_margin]" maxlength="2" value="<?php esc_attr_e( $jmig_options['item_margin'] ); ?>" />
+
+											<label class="description" for="jmig_option[item_margin]"><?php _e( 'px. Please DO NOT enter \'px\'. Just enter the number. Leave blank for default 1px margin.', 'jmig_plugin' ); ?></label>
+
+										</td>
+
+								</tr>
+
 							</table>
-					
+
+							<table class="form-table">
+
+								<h3>If you want to remove all CSS injected by jMIG, then click both the following boxes. It will also remove the custom margin from above if one was entered.</h3>
+
+									<tr valign="top">
+
+										<th scope="row"><strong>DO NOT allow jMIG to add any CSS that modifies your gallery or gallery items.</strong></th>
+
+											<td><input name="jmig_option[no_added_css]" type="checkbox" value="1" <?php checked( '1', (isset($jmig_options['no_added_css'])) ); ?> /></td>
+
+									</tr>
+
+									<tr valign="top">
+
+										<th scope="row"><strong>DO NOT</strong> allow jMIG to layout your gallery columns?</th>
+
+											<td><input name="jmig_option[fixed_layout]" type="checkbox" value="1" <?php checked( '1', (isset($jmig_options['fixed_layout'])) ); ?> /></td>
+
+									</tr>
+
+							</table>
+
 								<p class="submit"><input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" /></p>
+
 					</form>
-					
+
+						<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
+
+							<input type="hidden" name="cmd" value="_s-xclick">
+							<input type="hidden" name="hosted_button_id" value="NB46DBF4VJV5G">
+							<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+							<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+
+						</form>
+
 			</div>
-			
-	<?php	
-		
+
+	<?php
+
 		}
-		
-		
+
 		function jmig_options_validate($input) {
-			
-			global $select_options, $radio_options;
-				
+
+			if ( ! isset( $jmig_options['fixed_layout'] ) )$jmig_options['fixed_layout'] = null;
+
+				$jmig_options['fixed_layout'] = ( $jmig_options['fixed_layout'] == 1 ? 1 : 0 );
+
+			if ( ! isset( $jmig_options['no_added_css'] ) )$jmig_options['no_added_css'] = null;
+
+				$jmig_options['no_added_css'] = ( $jmig_options['no_added_css'] == 1 ? 1 : 0 );
+
+			$jmig_options['item_margin'] = wp_filter_nohtml_kses( $jmig_options['item_margin'] );
+
 				return $input;
 		}
 
@@ -96,169 +148,35 @@ if (is_admin()) {
 else {
 
 	// START USING MASONRY 3 OPTIONS INSTEAD OF MASONRY 2. SHOULD BE A LITTLE FASTER AND RESPONSIVE.
-	
+
 	if ($wp_version >= '3.9') {
-	
-		function jmig_css() {
-	
-			global $post;
-			global $wp_styles;
-				
-				if( has_shortcode( $post->post_content, 'gallery') ) {
 
-					wp_enqueue_style('jmig_stylesheet',
-					plugins_url( 'styles/jmig-masonry-v3.css' , __FILE__ ),
-					array(),
-					'2.1.6'
-					);
+		$three_dot_nine = plugin_dir_path( __FILE__ ) . "functions/three-dot-nine.php";
 
-						$jmig_options = get_option('jmig_option');
+			include_once($three_dot_nine);
 
-							if(!isset($jmig_options['fixed_layout'])) { 	
-
-								$thumbnail_width = get_option( 'thumbnail_size_w' );
-								$custom_css = '.gallery-item, .gallery-item img {width: ' . $thumbnail_width . 'px !important;}';
-		
-									wp_add_inline_style( 'jmig_stylesheet', $custom_css );
-							}
-								
-										
-								wp_enqueue_style( 'jmig-lte-IE9',
-								plugins_url( 'styles/jmig-lte-ie9.css' , __FILE__ ),
-								array(),
-								'2.1.6'
-								);
-										
-									$wp_styles->add_data( 'jmig-lte-IE9', 'conditional', 'lte IE 9' );
-										
-					
-				}
-
-		}
-
-			add_action( 'wp_enqueue_scripts', 'jmig_css', 99 );
-
-		function jmig_js() {
-			
-			global $post;
-			
-				if( has_shortcode( $post->post_content, 'gallery') ) {
-					
-					wp_register_script('masonryInit',
-					plugins_url( 'js/masonry-init-v3.js' , __FILE__ ),
-					array('masonry'),
-					'2.1.6', 
-					true);
-		      
-						wp_enqueue_script('masonryInit');
-						
-				}
-	  
-		}
-			
-			add_action( 'wp_enqueue_scripts', 'jmig_js');
-		
 	}
-	
-	//LEGACY CODE BELOW FOR WORDPRESS VERSIONS 3.8.X TO 3.6.X.
-	
-	elseif ($wp_version >= '3.6') {
-	
-		$jmig_options = get_option('jmig_option');
 
-			if(!isset($jmig_options['fixed_layout'])) { 
-					
-				function jmig_css() {
-		
-					global $post;
-		
-						if( has_shortcode( $post->post_content, 'gallery') ) {
-	
-							wp_enqueue_style('jmig_stylesheet',
-							plugins_url( 'styles/jmig-masonry-v2.css' , __FILE__ ),
-							array(),
-							'1.6'
-							);
-	    
-								$thumbnail_width = get_option( 'thumbnail_size_w' );
-								$custom_css = '.gallery-item, .gallery-item img {width: ' . $thumbnail_width . 'px !important;}';
-		
-									wp_add_inline_style( 'jmig_stylesheet', $custom_css );
-					
-						}
-	
-				}
-	
-					add_action( 'wp_enqueue_scripts', 'jmig_css', 99 );
-	
-			}
-	
-				function jmig_js() {
-					
-					global $post;
-					
-						if( has_shortcode( $post->post_content, 'gallery') ) {
-							
-							wp_register_script('masonryInit',
-							plugins_url( 'js/masonry-init-v2.js' , __FILE__ ),
-							array('jquery-masonry'),
-							'1.6', 
-							true);
-				      
-								wp_enqueue_script('masonryInit');
-								
-						}
-			  
-				}
-			
-					add_action( 'wp_enqueue_scripts', 'jmig_js');
-		
+	//LEGACY CODE BELOW FOR WORDPRESS VERSIONS 3.8.X TO 3.6.X.
+
+	elseif ($wp_version >= '3.6') {
+
+		$three_dot_six = plugin_dir_path( __FILE__ ) . "functions/three-dot-six.php";
+
+			include_once($three_dot_six);
+
 	}
 
 	//BELOW IS ONLY FOR WORDPRESS 3.5.X ...#oldmanriver
-			
+
 	else {
-	
-		$jmig_options = get_option('jmig_option');
-		
-			if(!isset($jmig_options['fixed_layout'])) {
-			
-				function jmig_css() 
-				
-					{
-						wp_enqueue_style('jmig_stylesheet',
-						plugins_url( 'styles/jmig-masonry-v2.css' , __FILE__ ),
-						array(),
-						'1.6'
-						);
-		        
-							$thumbnail_width = get_option( 'thumbnail_size_w' );
-							$custom_css = '.gallery-item, .gallery-item img {width: ' . $thumbnail_width . 'px !important;}';
-				
-								wp_add_inline_style( 'jmig_stylesheet', $custom_css );
-							
-					}
-		
-						add_action( 'wp_enqueue_scripts', 'jmig_css', 99 );
-			
-			}
-			
-				function masonry_init() {
-				
-						wp_register_script('masonryInit',
-						plugins_url( 'js/masonry-init-v2.js' , __FILE__ ),
-						array('jquery-masonry'),
-				        '1.6', 
-						true);
-		        
-							wp_enqueue_script('masonryInit');
-		     
-				}
-		  
-					add_action('wp_enqueue_scripts', 'masonry_init');
-	
+
+		$three_dot_five = plugin_dir_path( __FILE__ ) . "functions/three-dot-five.php";
+
+			include_once($three_dot_five);
+
 	}
-  
+
 }
 
 ?>
--- readme.txt
+++ readme.txt
@@ -4,32 +4,33 @@ Donate link: http://bit.ly/1jzZKCu
 Tags: gallery, jquery masonry, masonry
 Requires at least: 3.5
 Tested up to: 3.9.1
-Stable tag: 2.1.6
+Stable tag: 2.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Applies Masonry to native WordPress image galleries. Works best on galleries without 1:1 scaled thumbnails.
+Adds a Masonry layout to all galleries that are using the WordPress [gallery] shortcode.
 
 == Description ==
 
 [See Demo](http://willrees.com/jquery-masonry-wordpress-gallery-plugin-demo/?utm_source=wordpressorg&utm_medium=link&utm_campaign=jmig "See Demo")
 
-[Try Beta](http://willrees.com/2014/05/jquery-masonry-image-gallery-beta-testing/?utm_source=wordpressorg&utm_medium=link&utm_campaign=jmig "Try Beta")
+Adds a Masonry layout to all galleries that are using the WordPress [gallery] shortcode. Looks best on galleries that are not using 1:1 ratio thumbnails.
 
-Applies Masonry to native WordPress image galleries. Masonry is included in WordPress, use it for image galleries. Works best on galleries <strong>without</strong> 1:1 scaled thumbnails.
+<strong>Please note:</strong> By default this plugin includes CSS that will overwrite the existing gallery layout. However, you can turn off some or all of the jMIG layout CSS on the options page under Settings.
 
-<strong>Please note:</strong> This plugin will include 1 line of CSS in your theme. The only CSS injected is the thumbnail width that is taken directly from the Media options page. This will not alter gallery thumbnail styles such as border, padding, etc. This 1 line of CSS helps eliminate image overlap in a responsive theme during window resize. You can turn this off in the options page if necessary.
-
-This plugin also includes 1 CSS file that is for animation purposes only (Chrome, Firefox, Safari and IE 10+). The entire gallery will fade in using a CSS3 animation once all the images have loaded. This include will not alter gallery thumbnail styles such as border, padding, etc. IE 8 and IE 9 will work, but will not fade in.
+This plugin also includes 1 CSS file that is for animation purposes only (Chrome, Firefox, Safari and IE 10+). The entire gallery will fade in using a CSS3 animation once all the images have loaded. IE 8 and IE 9 will work, but will not fade in.
 
 == Installation ==
 
 1. Upload `jquery-masonry-image-gallery` folder to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
-3. If you are using a fixed layout and/or want Masonry to respect the column count in the gallery shortcode, then go to Settings -> Jmig options and make sure the box is checked and save.
+3. Go to Settings -> Jmig options if you need to change settings
 4. That's it!
 
 == Frequently asked questions ==
+
+= All of my galleries looked different before I upgraded this plugin to 2.2. Can I change it back? =
+Yes. Go to Dashboard -> Settings -> JMIG Options. Check the box DO NOT allow jMIG to add any CSS that modifies your gallery or gallery items, then save.
 
 = The specified amount of columns in the gallery short code is not working. What's wrong?  =
 
@@ -37,7 +38,7 @@ If you are using a fixed layout and/or want Masonry to respect the column count 
 
 = All my thumbnails are the same size so they don't stack. Can I fix this? =
 
-Yes. It's actually really easy. Go to the Dashboard -> Settings -> Media. Make sure "Crop thumbnail to exact dimensions (normally thumbnails are proportional)" is unchecked. You can also change the size of thumbnails as well. [Then install the Regenerate Images plugin](http://wordpress.org/plugins/regenerate-thumbnails/) and run it. Now your gallery images will stack.
+Yes. It's actually really easy. Go to the Dashboard -> Settings -> Media. Make sure "Crop thumbnail to exact dimensions (normally thumbnails are proportional)" is unchecked. You can also change the size of thumbnails as well. I like to set my thumbnail width between 150px and 250px, then set the height to 9999px. [Then install the Regenerate Images plugin](http://wordpress.org/plugins/regenerate-thumbnails/) and run it. Now your gallery images will stack.
 
 == Screenshots ==
 
@@ -46,6 +47,8 @@ Yes. It's actually really easy. Go to the Dashboard -> Settings -> Media. Make s
 2. Native WordPress Gallery not using jQuery Masonry
 
 == Changelog ==
+2.2 - Adds in new layout CSS with the option to turn it off for backwards compatibility
+
 2.1.6 - Fixed a minor issue with server side caching and IE 9 and below
 
 2.1.5 - Better support for WordPress 3.9 (Vanilla JS now supports multiple galleries on a page or post)
@@ -73,6 +76,8 @@ Yes. It's actually really easy. Go to the Dashboard -> Settings -> Media. Make s
 0.1 - First Release
 
 == Upgrade Notice ==
+2.2 - Adds in new layout CSS with the option to turn it off for backwards compatibility
+
 2.1.6 - Fixed a minor issue with server side caching and IE 9 and below
 
 2.1.5 - Better support for WordPress 3.9 (Vanilla JS now supports multiple galleries on a page or post)
