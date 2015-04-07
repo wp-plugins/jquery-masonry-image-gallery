bd07eea58e91e52a858022b27da74a64c94d15ea
phoenixMagoo <phoenixMagoo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-07 22:31:59 +0000

    big lazy load push
    git-svn-id: https://plugins.svn.wordpress.org/jquery-masonry-image-gallery/trunk@1129970 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- functions/three-dot-six.php
+++ functions/three-dot-six.php
@@ -1,88 +1,90 @@
 <?php
 
-	$jmig_options = get_option('jmig_option');
+defined('ABSPATH') or die("No script kiddies please!");
 
-		function jmig_css() {
+$jmig_options = get_option('jmig_option');
 
-			global $post;
-			global $wp_styles;
+function jmig_css()
+{
 
-				if( has_shortcode( $post->post_content, 'gallery') ) {
+    global $wp_styles;
 
-					$jmig_options = get_option('jmig_option');
+    $jmig_options = get_option('jmig_option');
 
-						if(!isset($jmig_options['no_added_css'])) {
+    if (!isset($jmig_options['no_added_css'])) {
 
-							wp_enqueue_style('jmig_stylesheet_layout',
-							plugins_url( '/styles/jmig-masonry-layout.css' , dirname(__FILE__) ),
-							array(),
-							'2.2'
-							);
-						}
+        wp_enqueue_style('jmig_stylesheet_layout',
+            plugins_url('/styles/jmig-masonry-layout.css', dirname(__FILE__)),
+            array(),
+            '3.0'
+        );
+    }
 
-							wp_enqueue_style('jmig_stylesheet',
-								plugins_url( 'styles/jmig-masonry-v2.css' , dirname(__FILE__ ) ),
-								array(),
-								'2.2'
-							);
+    wp_enqueue_style('jmig_stylesheet',
+        plugins_url('styles/jmig-masonry-v2.css', dirname(__FILE__)),
+        array(),
+        '3.0'
+    );
 
-								if(!isset($jmig_options['fixed_layout'])) {
+    if (!isset($jmig_options['no_caption_css'])) {
 
-									$thumbnail_width = get_option( 'thumbnail_size_w' );
-									$custom_css_width = '.gallery-item, .gallery-item img, gallery-item a { width: ' . $thumbnail_width . 'px !important; max-width: ' . $thumbnail_width . 'px !important; min-width: ' . $thumbnail_width . 'px !important; }';
+    wp_enqueue_style('jmig_captions',
+        plugins_url('styles/jmig-maosnry-v3-captions.css', dirname(__FILE__)),
+        array(),
+        '3.0'
+    );
 
-										wp_add_inline_style( 'jmig_stylesheet', $custom_css_width );
+    }
 
-								}
+    if (!isset($jmig_options['fixed_layout'])) {
 
-									if (!isset($jmig_options['no_added_css'])) {
+        $thumbnail_width = get_option('thumbnail_size_w');
+        $custom_css_width = '.gallery-item, .gallery-item img, gallery-item a { width: ' . $thumbnail_width . 'px !important; max-width: ' . $thumbnail_width . 'px !important; min-width: ' . $thumbnail_width . 'px !important; }';
 
-										if ( $jmig_options['item_margin'] == NULL ) {
+        wp_add_inline_style('jmig_stylesheet', $custom_css_width);
 
-											$custom_css_margin = '.gallery-item {margin: 1px !important}';
+    }
 
-										}
+    if (!isset($jmig_options['no_added_css'])) {
 
-										else {
+        if ($jmig_options['item_margin'] == NULL) {
 
-											$custom_css_margin = '.gallery-item {margin: ' . $jmig_options['item_margin'] . 'px !important}';
+            $custom_css_margin = '.gallery-item {margin: 1px !important}';
 
-										}
+        } else {
 
-											wp_add_inline_style( 'jmig_stylesheet', $custom_css_margin );
-									}
+            $custom_css_margin = '.gallery-item {margin: ' . $jmig_options['item_margin'] . 'px !important}';
 
-											wp_enqueue_style( 'jmig-lte-IE9',
-											plugins_url( 'styles/jmig-lte-ie9.css' , dirname(__FILE__ ) ),
-											array(),
-											'2.2'
-											);
+        }
 
-												$wp_styles->add_data( 'jmig-lte-IE9', 'conditional', 'lte IE 9' );
+        wp_add_inline_style('jmig_stylesheet', $custom_css_margin);
+    }
 
-					}
+    wp_enqueue_style('jmig-lte-IE9',
+        plugins_url('styles/jmig-lte-ie9.css', dirname(__FILE__)),
+        array(),
+        '3.0'
+    );
 
-			}
+    $wp_styles->add_data('jmig-lte-IE9', 'conditional', 'lte IE 9');
 
-				add_action( 'wp_enqueue_scripts', 'jmig_css', 99 );
+}
 
-		function jmig_js() {
+add_action('wp_enqueue_scripts', 'jmig_css', 99);
 
-			global $post;
+function jmig_js()
+{
 
-				if( has_shortcode( $post->post_content, 'gallery') ) {
+    wp_register_script('masonryInit',
+        plugins_url('js/masonry-init-v2.js', dirname(__FILE__)),
+        array('jquery-masonry'),
+        '3.0',
+        true);
 
-					wp_register_script('masonryInit',
-					plugins_url( 'js/masonry-init-v2.js' , dirname(__FILE__) ),
-					array('jquery-masonry'),
-					'1.6',
-					true);
+    wp_enqueue_script('masonryInit');
 
-						wp_enqueue_script('masonryInit');
 
-				}
+}
 
-		}
-
-			add_action( 'wp_enqueue_scripts', 'jmig_js');
+add_action('wp_enqueue_scripts', 'jmig_js');
 ?>
